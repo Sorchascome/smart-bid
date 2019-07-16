738c4ddc2c2cb7faf8f632833ae7ca841ca57acd
Sorchascome <sorcishere@gmail.com>
2019-07-16 17:19:01 +0300

    minor fixes

--- client/src/components/TechList.js
+++ client/src/components/TechList.js
@@ -1,5 +1,5 @@
 import React from 'react'
 
 export default function TechList({ items }) {
-    return items.map((item) => (<span className="listitem hidden_alt slide"><span key={item} className='TechList'>{item}</span></span>))
+    return items.map((item) => (<span key={item} className="listitem hidden_alt slide"><span className='TechList'>{item}</span></span>))
 }

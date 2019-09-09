8487f8fc0038895e49f3166bfbd65cd6fd86e6d3
Sorchascome <sorcishere@gmail.com>
2019-09-09 18:03:07 +0300

    changes

--- client/src/components/TopPanel.js
+++ client/src/components/TopPanel.js
@@ -30,7 +30,7 @@ export default class TopPanel extends Component {
         e.preventDefault();
         let activatedLink = e.target;
         let gotoClass = e.target.getAttribute('value');
-        let gotoElemY = Math.abs(document.querySelector('.SectionOne').getBoundingClientRect().y - document.querySelector(gotoClass).getBoundingClientRect().y);
+        let gotoElemY = Math.abs(document.querySelector('.SectionOne').getBoundingClientRect().y - document.querySelector(gotoClass).getBoundingClientRect().y + (gotoClass === '.Technology.desk' ? 50 : 0));
         window.scroll({
             top: gotoElemY, 
             left: 0, 
@@ -56,7 +56,7 @@ export default class TopPanel extends Component {
         return (
             <div className='TopPanel slide hidden'>
                 <a className='top_panel_link' href="#" value=".AboutUs.desk" onClick={this.handleClick}>About us</a>
-                <a className='top_panel_link' href="#" value=".SectionThree" onClick={this.handleClick}>Technology</a>
+                <a className='top_panel_link' href="#" value=".Technology.desk" onClick={this.handleClick}>Technology</a>
                 <a className='top_panel_link' href="#" value=".OurProducts.desk" onClick={this.handleClick}>Products</a>
                 <a className='top_panel_link' href="#" value=".SectionFour.desk" onClick={this.handleClick}>Services</a>
                 <a className='top_panel_link' href="#" value=".section_footer.desk" onClick={this.handleClick}>Contact us</a>
